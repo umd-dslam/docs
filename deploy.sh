559a9c4ac9e556520035ae573d9417a253074d94
Gang Liao <gangliao@cs.umd.edu>
2019-01-19 05:00:56 -0500

    update travis ci

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,17 @@
 
 set -o errexit -o nounset
 
+rev=$(git rev-parse --short HEAD)
+
 echo 'Building...'
 mdbook build
 
 cd book
 
-touch .
+git init
+git config user.name "${U_NAME}"
+git config user.email "${U_EMAIL}"
 
-rev=$(git rev-parse --short HEAD)
 git add -A .
 git commit -m "rebuild pages at ${rev}"
-git push -q upstream HEAD:gh-pages
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:${P_BRANCH}
