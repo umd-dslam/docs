f374d4d2b59d233ed0b72f1960e4de91078cc748
Gang Liao <gangliao@cs.umd.edu>
2019-01-19 05:37:07 -0500

    update dir

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ set -o errexit -o nounset
 
 rev=$(git rev-parse --short HEAD)
 
+rm -rf book
+
 echo 'Building...'
 mdbook build
 
@@ -13,7 +15,7 @@ git init
 git config user.name "${U_NAME}"
 git config user.email "${U_EMAIL}"
 
-git add -A .
+git add .
 git commit -m "rebuild pages at ${rev}"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:${P_BRANCH}
 
